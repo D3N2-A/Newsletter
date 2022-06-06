b0b1525dd3197f05cb2c2b8872f38c29a0d459cb
D3N2-A <anmolsharma999937@gmail.com>
2022-06-06 20:55:52 +0530

    mailchimp data object

--- app.js
+++ app.js
@@ -4,8 +4,35 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
+app.post("/", function (req, res) {
+  // console.log(req);
+  var firstName = req.body.firstName;
+  var secondName = req.body.secondName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: secondName,
+        },
+      },
+    ],
+  };
+
+  var JSONdata = JSON.stringify(data);
+});
 
 app.listen(3000, function () {
   console.log("Server started at port 3000");
 });
+
+//d886c1ea28915ae21acaf6915445a202-us8
+// 68c0aa09b7
